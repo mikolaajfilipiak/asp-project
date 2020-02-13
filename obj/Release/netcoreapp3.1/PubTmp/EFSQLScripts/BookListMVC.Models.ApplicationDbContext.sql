1424d5392ecaf4d07ef7515c1ab352bce753bf93
Precho <kamilnatonek@gmail.com>
2020-02-13 21:16:12 +0100

    initial comit

---
+++ obj/Release/netcoreapp3.1/PubTmp/EFSQLScripts/BookListMVC.Models.ApplicationDbContext.sql
@@ -0,0 +1,32 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200123002027_AddBookToDb')
+BEGIN
+    CREATE TABLE [Books] (
+        [Id] int NOT NULL IDENTITY,
+        [Name] nvarchar(max) NOT NULL,
+        [Author] nvarchar(max) NULL,
+        [ISBN] nvarchar(max) NULL,
+        CONSTRAINT [PK_Books] PRIMARY KEY ([Id])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200123002027_AddBookToDb')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20200123002027_AddBookToDb', N'3.1.1');
+END;
+
+GO
+
---
+++ wwwroot/js/library.js
@@ -0,0 +1,66 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#DT_load').DataTable({
+        "ajax": {
+            "url": "/books/all/",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "name", "width": "20%" },
+            { "data": "author", "width": "20%" },
+            { "data": "isbn", "width": "20%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `<div class="text-center">
+                        <a href="/book/upsert?id=${data}" class='btn btn-success text-white' style='cursor:pointer; width:70px;'>
+                            Edytuj
+                        </a>
+                        &nbsp;
+                        <a class='btn btn-danger text-white' style='cursor:pointer; width:70px;'
+                            onclick=Delete('/book/delete?id='+${data})>
+                            Usuń
+                        </a>
+                        </div>`;
+                }, "width": "40%"
+            }
+        ],
+        "language": {
+            "url": "https://cdn.datatables.net/plug-ins/1.10.20/i18n/Polish.json",
+            "emptyTable": "Brak wyników."
+        },
+        "width": "100%"
+    });
+}
+
+function Delete(url) {
+    swal({
+        title: "Jesteś pewien?",
+        text: "Ta akcja jest nieodwracalna",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
+    });
+}
